9624f94c5d1548b826127be251c9ec0f3d285a2a
yskeerthi <119730796+yskeerthi@users.noreply.github.com>
2023-08-26 20:51:10 +0530

    import java.util.Scanner; public class pal {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         int temp=n,rev=0;         while(temp>0)         {             int r=temp%10;             rev=rev*10+r;             temp/=10;         }         System.out.println(rev);     } }

---
+++ reverse_of_a_numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class pal
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int temp=n,rev=0;
+        while(temp>0)
+        {
+            int r=temp%10;
+            rev=rev*10+r;
+            temp/=10;
+        }
+        System.out.println(rev);
+    }
+}
